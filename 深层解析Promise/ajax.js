33f0fc0d965abe28c36be53d315cdc5270fafe00
xiaoshuai <1378334435@qq.com>
2022-02-05 14:03:20 +0800

    ajax封装

---
+++ 深层解析Promise/ajax.js
@@ -0,0 +1,20 @@
+function ajax(url) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.send();
+        xhr.onload = function () {
+            if (this.status == 200) {
+                resolve(JSON.parse(this.response))
+            }
+            else {
+                console.log(this.status)
+                reject("加载失败")
+            }
+        }
+        xhr.onerror = function () {
+            reject(this)
+        }
+    })
+
+}
